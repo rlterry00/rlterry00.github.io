e976775217217c104ecb4a67fb0e0a7f78147665
Ramon <Ramon@Ramons-MacBook-Pro.local>
2017-10-19 06:19:20 -0400

    https

--- assets/js/jquery.easypiechart.js
+++ assets/js/jquery.easypiechart.js
@@ -1,11 +1,4 @@
-/**!
- * easyPieChart
- * Lightweight plugin to render simple, animated and retina optimized pie charts
- *
- * @license 
- * @author Robert Fleischmann <rendro87@gmail.com> (http://robert-fleischmann.de)
- * @version 2.1.6
- **/
+
 
 (function(root, factory) {
     if(typeof exports === 'object') {
